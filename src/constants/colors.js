985fff1efb980bbee7e25b84fb1c666cf508a287
lukasquense <lukas.quense@gmail.com>
2021-04-18 20:30:32 -0400

    feat(constants): colors constant file added

---
+++ src/constants/colors.js
@@ -0,0 +1,37 @@
+
+const COLORS = {
+    ORANGE: '#FF452B',
+    LIGHT_ORANGE: '#FFDBD0',
+    WHITE: '#ffffff',
+    GREY: '#424242',
+    LIGHT_GREY: '#616161',
+    BLACK: '#212121',
+    SUCCESS: '#3f8600',
+  };
+
+
+export const TYPE_COLORS = {
+  'normal': '#a4acaf',
+  'fighting': '#d56723',
+  'flying': '#3dc7ef',
+  'poison': '#b97fc9',
+  'ground': '#f7ed3f',
+  'bug': '#729f3f',
+  'ghost': '7b62a3',
+  'steel': '#9eb7b8',
+  'fire': '#fd7d24',
+  'water': '#4592c4',
+  'grass': '#9bcc50',
+  'rock': '#a38c21',
+  'electric': '#eed535',
+  'fairy': '#fdb9e9',
+  'phychic': '#f366b9',
+  'ice': '#51c4e7',
+  'dragon': '#53a4cf',
+  'dark': '#707070',
+  'unknown': '#707070',
+  'shadow': '#707070',
+}
+  
+  export default COLORS;
+  
